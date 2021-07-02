9681654e468402b614e8e77d64d9c833b5640c22
Your Name <you@example.com>
2021-07-02 21:01:13 +0530

    Created Notes and can add or delete note

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('',views.home, name='home'),
     path('notes',views.notes, name='notes'),
+    path('delete_note/<int:pk>',views.delete_note, name='delete-note'),
     path('books', views.books, name='books'),
     path('notesDetails', views.notes_detail, name='notesDetails')
 ]
